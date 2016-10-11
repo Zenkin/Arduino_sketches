ec73a7aa14d75701e7b5e1e9a44b187d37d91ddc
Zenkin <zenkinartem1997@gmail.com>
2016-10-11 22:59:22 +0300

    Add files via upload

---
+++ work_with_motors/Engine_Start.ino
@@ -0,0 +1,29 @@
+void Engine_start(int Speed, char Engine, char Direction) {
+  int PWM = round(Speed * 2.55);
+  if (Engine == 'L') {
+    if (Speed != 0) {
+      if (Direction == 'F') {
+        LeftMotor.setSpeed(PWM);
+        LeftMotor.run(FORWARD);    
+      }
+      if (Direction == 'B') {
+        LeftMotor.setSpeed(PWM);
+        LeftMotor.run(BACKWARD);  
+      }
+    }
+    if (Speed == 0) { LeftMotor.run(RELEASE); }   
+  }
+  if (Engine == 'R') {
+    if (Speed != 0) {
+      if (Direction == 'F') {
+        RightMotor.setSpeed(PWM);
+        RightMotor.run(FORWARD);    
+      }
+      if (Direction == 'B') {
+        RightMotor.setSpeed(PWM);
+        RightMotor.run(BACKWARD);  
+      }
+    }
+    if (Speed == 0) { RightMotor.run(RELEASE); }     
+  }
+}
---
+++ work_with_motors/work_with_motors.ino
@@ -0,0 +1,27 @@
+/*
+
+  Function Engine_start(int Speed, char Engine, char Direction);
+  Speed: 0 - 100 in percent;
+  Engine: L - left motor, R - right motor;
+  Direction: F - forward, B - backward;
+    
+*/
+#include <AFMotor.h>
+
+const byte left_motor = 1;
+const byte right_motor = 2;
+
+AF_DCMotor RightMotor(right_motor);
+AF_DCMotor LeftMotor(left_motor);
+
+void Engine_start(int Speed, char Engine, char Direction);
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  Engine_start(100, 'R', 'F');
+  Engine_start(100, 'R', 'F');  
+}
+
