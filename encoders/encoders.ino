13029cda0aabf648f8ba025c23dfbdc6963e84b9
Zenkin <zenkinartem1997@gmail.com>
2016-10-11 22:58:06 +0300

    Add files via upload

---
+++ encoders/encoders.ino
@@ -0,0 +1,35 @@
+const int out_pin = 12;
+const int wait_time = 1;
+
+long rotation_count(int encoder_pin);
+long i, angle;
+byte current_angle, previous_angle;
+
+void setup() 
+
+{
+  Serial.begin(9600);
+  pinMode(out_pin, INPUT);
+}
+
+void loop() 
+{
+  long rot_count;
+  
+  rot_count = rotation_count(out_pin);
+  Serial.print(rot_count);
+  Serial.println(" deg");
+  delay(wait_time);
+}
+
+long rotation_count(int encoder_pin)
+{
+  current_angle = digitalRead(encoder_pin);
+  if (previous_angle != current_angle) 
+  {
+    i++;
+    angle = i * 9;
+  }
+  previous_angle = digitalRead(encoder_pin);
+  return angle;  
+}
